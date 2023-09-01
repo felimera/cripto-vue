036218f50c839d5007ab94f699109fdc233bed9a
andres.mera <feli041089@gmail.com>
2023-09-01 08:19:24 -0500

    Leyendo lo valores que deseamos cotizar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, reactive, onMounted } from 'vue';
 
 const monedas = ref([
   { codigo: 'USD', texto: 'Dolar de Estados Unidos' },
@@ -9,6 +9,11 @@ const monedas = ref([
 ]);
 
 const criptoMonedas = ref([]);
+
+const cotizar = reactive({
+  moneda: '',
+  criptomoneda: ''
+});
 
 onMounted(() => {
   const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
@@ -27,7 +32,7 @@ onMounted(() => {
 
         <div class="campo">
           <label for="moneda">Moneda:</label>
-          <select id="moneda">
+          <select id="moneda" v-model="cotizar.moneda">
             <option value="">-- Selecciona --</option>
             <option v-for="moneda in monedas" :value="moneda.codigo">
               {{ moneda.texto }}
@@ -36,7 +41,7 @@ onMounted(() => {
         </div>
         <div class="campo">
           <label for="cripto">Criptomoneda:</label>
-          <select id="cripto">
+          <select id="cripto" v-model="cotizar.criptomoneda">
             <option value="">-- Selecciona --</option>
             <option v-for="criptomonada in criptoMonedas" :value="criptomonada.CoinInfo.Name">
               {{ criptomonada.CoinInfo.FullName }}
